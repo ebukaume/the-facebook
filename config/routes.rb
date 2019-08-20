430356be5708278501d3e2ad93e83c7be5254f5d
Ebuka Umeokonkwo <ebukaume@gmail.com>
2019-08-20 16:14:35 +0100

    adds initial basic routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resource :user, except: [:index]
+
+  resource :post do
+    resources :comments, except: [:index, :new, :show]
+    resource :likes, only: [:create, :destroy]
+  end
+
+  get ":user_id/friends", to: "friends#index"
 end
