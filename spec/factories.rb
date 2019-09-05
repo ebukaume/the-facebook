6852d58ff732f4bcda9cfa0631a3854a52b4d68c
Ebuka Umeokonkwo <ebukaume@gmail.com>
2019-09-05 13:06:00 +0100

    adds feature specs

--- spec/factories.rb
+++ spec/factories.rb
@@ -4,7 +4,7 @@ FactoryBot.define do
   factory :user do
     first_name { Faker::Name.first_name }
     last_name { Faker::Name.last_name }
-    email { Faker::Internet.email }
+    sequence(:email) { |n| "user#{n}@email.com" }
     password { Faker::Internet.password }
     sex { %w[Male Female Custom].sample }
     dob { 20.years.ago }
