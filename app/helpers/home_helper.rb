63f98091b68e6772bebd66c1a1ec48c247cc62f2
Ebuka Umeokonkwo <ebukaume@gmail.com>
2019-09-07 00:56:01 +0100

    fixes linting errors

--- app/helpers/home_helper.rb
+++ app/helpers/home_helper.rb
@@ -1,13 +1,7 @@
 # frozen_string_literal: true
 
 module HomeHelper
-
-  def data_class_for(type)
-    if type == :notification
-      return '' if false
-    elsif type == :friend_request
-      return '' if false
-    end
+  def data_class_for(*)
     'no-data'
   end
 end
