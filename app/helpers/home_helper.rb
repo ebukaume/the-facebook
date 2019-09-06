e8083f0df7e843c67ae13a646b234aa3472acea0
Ebuka Umeokonkwo <ebukaume@gmail.com>
2019-09-07 00:49:23 +0100

    makes minor UI changes on the header

--- app/helpers/home_helper.rb
+++ app/helpers/home_helper.rb
@@ -1,4 +1,13 @@
 # frozen_string_literal: true
 
 module HomeHelper
+
+  def data_class_for(type)
+    if type == :notification
+      return '' if false
+    elsif type == :friend_request
+      return '' if false
+    end
+    'no-data'
+  end
 end
